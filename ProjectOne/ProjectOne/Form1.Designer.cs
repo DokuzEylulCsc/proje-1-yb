144cfbd6a048059bf4876ab2c52e0d039a2162e5
yavuzgedik <yavuzgedik@outlook.com>
2019-04-08 13:11:29 +0300

    Form Arayüzü ve Entegrasyonlar Yapıldı.

--- ProjectOne/ProjectOne/Form1.Designer.cs
+++ ProjectOne/ProjectOne/Form1.Designer.cs
@@ -28,13 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxOdalar = new System.Windows.Forms.ListBox();
+            this.labelList = new System.Windows.Forms.Label();
+            this.labelOtelAdi = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerBas = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerBit = new System.Windows.Forms.DateTimePicker();
+            this.btnOdaSorgula = new System.Windows.Forms.Button();
+            this.groupBoxSecenekler = new System.Windows.Forms.GroupBox();
+            this.labelDoluOdalar = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxYatakTipi = new System.Windows.Forms.ComboBox();
+            this.checkBoxHavuzManzarasi = new System.Windows.Forms.CheckBox();
+            this.checkBoxDenizManzarasi = new System.Windows.Forms.CheckBox();
+            this.btnRezervasyonYap = new System.Windows.Forms.Button();
+            this.btnRezervasyonList = new System.Windows.Forms.Button();
+            this.btnTumRez = new System.Windows.Forms.Button();
+            this.btnDoluluk = new System.Windows.Forms.Button();
+            this.groupBoxSecenekler.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxOdalar
+            // 
+            this.listBoxOdalar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxOdalar.FormattingEnabled = true;
+            this.listBoxOdalar.ItemHeight = 20;
+            this.listBoxOdalar.Location = new System.Drawing.Point(518, 57);
+            this.listBoxOdalar.Name = "listBoxOdalar";
+            this.listBoxOdalar.Size = new System.Drawing.Size(270, 404);
+            this.listBoxOdalar.TabIndex = 0;
+            this.listBoxOdalar.DoubleClick += new System.EventHandler(this.listBoxOdalar_DoubleClick);
+            // 
+            // labelList
+            // 
+            this.labelList.AutoSize = true;
+            this.labelList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelList.Location = new System.Drawing.Point(514, 15);
+            this.labelList.Name = "labelList";
+            this.labelList.Size = new System.Drawing.Size(168, 20);
+            this.labelList.TabIndex = 1;
+            this.labelList.Text = "Odalar (Çift Tık: Detay)";
+            // 
+            // labelOtelAdi
+            // 
+            this.labelOtelAdi.AutoSize = true;
+            this.labelOtelAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelOtelAdi.Location = new System.Drawing.Point(105, 15);
+            this.labelOtelAdi.Name = "labelOtelAdi";
+            this.labelOtelAdi.Size = new System.Drawing.Size(42, 20);
+            this.labelOtelAdi.TabIndex = 2;
+            this.labelOtelAdi.Text = "XXX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(22, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Otel Adı : ";
+            // 
+            // dateTimePickerBas
+            // 
+            this.dateTimePickerBas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePickerBas.Location = new System.Drawing.Point(20, 36);
+            this.dateTimePickerBas.Name = "dateTimePickerBas";
+            this.dateTimePickerBas.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePickerBas.TabIndex = 4;
+            this.dateTimePickerBas.ValueChanged += new System.EventHandler(this.dateTimePickerBas_ValueChanged);
+            // 
+            // dateTimePickerBit
+            // 
+            this.dateTimePickerBit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePickerBit.Location = new System.Drawing.Point(237, 36);
+            this.dateTimePickerBit.Name = "dateTimePickerBit";
+            this.dateTimePickerBit.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePickerBit.TabIndex = 5;
+            // 
+            // btnOdaSorgula
+            // 
+            this.btnOdaSorgula.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOdaSorgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOdaSorgula.ForeColor = System.Drawing.Color.White;
+            this.btnOdaSorgula.Location = new System.Drawing.Point(22, 164);
+            this.btnOdaSorgula.Name = "btnOdaSorgula";
+            this.btnOdaSorgula.Size = new System.Drawing.Size(415, 46);
+            this.btnOdaSorgula.TabIndex = 6;
+            this.btnOdaSorgula.Text = "Oda Sorgula";
+            this.btnOdaSorgula.UseVisualStyleBackColor = false;
+            this.btnOdaSorgula.Click += new System.EventHandler(this.btnOdaSorgula_Click);
+            // 
+            // groupBoxSecenekler
+            // 
+            this.groupBoxSecenekler.Controls.Add(this.labelDoluOdalar);
+            this.groupBoxSecenekler.Controls.Add(this.label2);
+            this.groupBoxSecenekler.Controls.Add(this.comboBoxYatakTipi);
+            this.groupBoxSecenekler.Controls.Add(this.checkBoxHavuzManzarasi);
+            this.groupBoxSecenekler.Controls.Add(this.checkBoxDenizManzarasi);
+            this.groupBoxSecenekler.Controls.Add(this.dateTimePickerBas);
+            this.groupBoxSecenekler.Controls.Add(this.btnOdaSorgula);
+            this.groupBoxSecenekler.Controls.Add(this.dateTimePickerBit);
+            this.groupBoxSecenekler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBoxSecenekler.Location = new System.Drawing.Point(26, 47);
+            this.groupBoxSecenekler.Name = "groupBoxSecenekler";
+            this.groupBoxSecenekler.Size = new System.Drawing.Size(465, 265);
+            this.groupBoxSecenekler.TabIndex = 7;
+            this.groupBoxSecenekler.TabStop = false;
+            this.groupBoxSecenekler.Text = "Oda Seçenekleri";
+            // 
+            // labelDoluOdalar
+            // 
+            this.labelDoluOdalar.AutoSize = true;
+            this.labelDoluOdalar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDoluOdalar.Location = new System.Drawing.Point(27, 223);
+            this.labelDoluOdalar.Name = "labelDoluOdalar";
+            this.labelDoluOdalar.Size = new System.Drawing.Size(0, 20);
+            this.labelDoluOdalar.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(16, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Yatak Tipi : ";
+            // 
+            // comboBoxYatakTipi
+            // 
+            this.comboBoxYatakTipi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxYatakTipi.FormattingEnabled = true;
+            this.comboBoxYatakTipi.Location = new System.Drawing.Point(115, 115);
+            this.comboBoxYatakTipi.Name = "comboBoxYatakTipi";
+            this.comboBoxYatakTipi.Size = new System.Drawing.Size(322, 28);
+            this.comboBoxYatakTipi.TabIndex = 10;
+            this.comboBoxYatakTipi.Tag = "";
+            // 
+            // checkBoxHavuzManzarasi
+            // 
+            this.checkBoxHavuzManzarasi.AutoSize = true;
+            this.checkBoxHavuzManzarasi.Location = new System.Drawing.Point(237, 79);
+            this.checkBoxHavuzManzarasi.Name = "checkBoxHavuzManzarasi";
+            this.checkBoxHavuzManzarasi.Size = new System.Drawing.Size(150, 24);
+            this.checkBoxHavuzManzarasi.TabIndex = 9;
+            this.checkBoxHavuzManzarasi.Text = "Havuz Manzarası";
+            this.checkBoxHavuzManzarasi.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxDenizManzarasi
+            // 
+            this.checkBoxDenizManzarasi.AutoSize = true;
+            this.checkBoxDenizManzarasi.Location = new System.Drawing.Point(20, 79);
+            this.checkBoxDenizManzarasi.Name = "checkBoxDenizManzarasi";
+            this.checkBoxDenizManzarasi.Size = new System.Drawing.Size(146, 24);
+            this.checkBoxDenizManzarasi.TabIndex = 8;
+            this.checkBoxDenizManzarasi.Text = "Deniz Manzarası";
+            this.checkBoxDenizManzarasi.UseVisualStyleBackColor = true;
+            // 
+            // btnRezervasyonYap
+            // 
+            this.btnRezervasyonYap.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnRezervasyonYap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRezervasyonYap.ForeColor = System.Drawing.Color.White;
+            this.btnRezervasyonYap.Location = new System.Drawing.Point(26, 318);
+            this.btnRezervasyonYap.Name = "btnRezervasyonYap";
+            this.btnRezervasyonYap.Size = new System.Drawing.Size(166, 94);
+            this.btnRezervasyonYap.TabIndex = 11;
+            this.btnRezervasyonYap.Text = "Rezervasyon Yap";
+            this.btnRezervasyonYap.UseVisualStyleBackColor = false;
+            this.btnRezervasyonYap.Click += new System.EventHandler(this.btnRezervasyonYap_Click);
+            // 
+            // btnRezervasyonList
+            // 
+            this.btnRezervasyonList.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnRezervasyonList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRezervasyonList.ForeColor = System.Drawing.Color.White;
+            this.btnRezervasyonList.Location = new System.Drawing.Point(206, 318);
+            this.btnRezervasyonList.Name = "btnRezervasyonList";
+            this.btnRezervasyonList.Size = new System.Drawing.Size(285, 46);
+            this.btnRezervasyonList.TabIndex = 12;
+            this.btnRezervasyonList.Text = "Seçili Tarih Rezervasyonları";
+            this.btnRezervasyonList.UseVisualStyleBackColor = false;
+            this.btnRezervasyonList.Click += new System.EventHandler(this.btnRezervasyonList_Click);
+            // 
+            // btnTumRez
+            // 
+            this.btnTumRez.BackColor = System.Drawing.Color.Green;
+            this.btnTumRez.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTumRez.ForeColor = System.Drawing.Color.White;
+            this.btnTumRez.Location = new System.Drawing.Point(206, 370);
+            this.btnTumRez.Name = "btnTumRez";
+            this.btnTumRez.Size = new System.Drawing.Size(285, 42);
+            this.btnTumRez.TabIndex = 13;
+            this.btnTumRez.Text = "Tüm Rezervasyonlar";
+            this.btnTumRez.UseVisualStyleBackColor = false;
+            this.btnTumRez.Click += new System.EventHandler(this.btnTumRez_Click);
+            // 
+            // btnDoluluk
+            // 
+            this.btnDoluluk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnDoluluk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDoluluk.ForeColor = System.Drawing.Color.White;
+            this.btnDoluluk.Location = new System.Drawing.Point(26, 418);
+            this.btnDoluluk.Name = "btnDoluluk";
+            this.btnDoluluk.Size = new System.Drawing.Size(465, 43);
+            this.btnDoluluk.TabIndex = 14;
+            this.btnDoluluk.Text = "Doluluk Oranı";
+            this.btnDoluluk.UseVisualStyleBackColor = false;
+            this.btnDoluluk.Click += new System.EventHandler(this.btnDoluluk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(813, 468);
+            this.Controls.Add(this.btnDoluluk);
+            this.Controls.Add(this.btnTumRez);
+            this.Controls.Add(this.btnRezervasyonList);
+            this.Controls.Add(this.btnRezervasyonYap);
+            this.Controls.Add(this.groupBoxSecenekler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelOtelAdi);
+            this.Controls.Add(this.labelList);
+            this.Controls.Add(this.listBoxOdalar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxSecenekler.ResumeLayout(false);
+            this.groupBoxSecenekler.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDoluluk;
+        private System.Windows.Forms.Button btnTumRez;
+        private System.Windows.Forms.Button btnRezervasyonList;
+        private System.Windows.Forms.Button btnRezervasyonYap;
+        private System.Windows.Forms.GroupBox groupBoxSecenekler;
+        private System.Windows.Forms.Label labelDoluOdalar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxYatakTipi;
+        private System.Windows.Forms.CheckBox checkBoxHavuzManzarasi;
+        private System.Windows.Forms.CheckBox checkBoxDenizManzarasi;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBas;
+        private System.Windows.Forms.Button btnOdaSorgula;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelOtelAdi;
+        private System.Windows.Forms.Label labelList;
+        private System.Windows.Forms.ListBox listBoxOdalar;
     }
 }
 
--- ProjectOne/ProjectOne/Form1.cs
+++ ProjectOne/ProjectOne/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjectOne.Models;
+using ProjectOne.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,190 @@ namespace ProjectOne
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Otel otel;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePickerBas.Value = DateTime.Now;
+            dateTimePickerBit.Value = DateTime.Now.AddDays(1);
+
+            comboBoxYatakTipi.DataSource = Enum.GetValues(typeof(YatakTipi));
+
+            otel = new Otel("Gedik Otel", "Seferihisar İzmir", OtelTipi.Otel);
+            labelOtelAdi.Text = otel.Adi;
+        }
+
+        private void btnOdaSorgula_Click(object sender, EventArgs e)
+        {
+            labelList.Text = "Uygun Odalar";
+            labelDoluOdalar.Text = "Dolu Odalar: ";
+            listBoxOdalar.Items.Clear();
+
+            var date1 = dateTimePickerBas.Value;
+            var date2 = dateTimePickerBit.Value;
+            var denizManzarasi = checkBoxDenizManzarasi.Checked;
+            var havuzManzarasi = checkBoxHavuzManzarasi.Checked;
+            var yatakTipi = comboBoxYatakTipi.SelectedItem;
+
+            foreach (var oda in otel.Odalari)
+            {
+                if (oda.DenizManzarasi == denizManzarasi && oda.HavuzManzarasi == havuzManzarasi && Enum.GetName(typeof(YatakTipi), oda.YatakTipi) == yatakTipi.ToString())
+                {
+                    if (oda.Rezervasyonlar != null && oda.Rezervasyonlar.Count > 0)
+                    {
+                        // Rezervasyon kontrolü (Oda Müsait mi?)
+                        var rezervasyonDurumu = otel.RezervasyonDurumu(oda, date1);
+
+                        if (!rezervasyonDurumu)
+                        {
+                            listBoxOdalar.Items.Add(oda);
+                        }
+                        else
+                        {
+                            labelDoluOdalar.Text += oda.Numara + ",";
+                        }
+                    }
+                    else
+                    {
+                        listBoxOdalar.Items.Add(oda);
+                    }
+                }
+            }
+
+            if (listBoxOdalar.Items.Count == 0)
+            {
+                MessageBox.Show("Uygun Oda Bulunamadı!");
+            }
+
+        }
+
+        private void btnRezervasyonYap_Click(object sender, EventArgs e)
+        {
+            if (listBoxOdalar.SelectedItem == null)
+            {
+                MessageBox.Show("Oda Seçiniz!");
+            }
+            else
+            {
+                var oda = listBoxOdalar.SelectedItem as Oda;
+
+                //Rezervasyon Yap
+
+                otel.RezervasyonYap(oda, new Rezervasyon()
+                {
+                    GirisTarihi = dateTimePickerBas.Value,
+                    CikisTarihi = dateTimePickerBit.Value,
+                    OdaNumarasi = oda.Numara,
+                    Misafirler = new List<Misafir>()
+                    {
+                        new Misafir() { Ad = "John Doe " + oda.Numara, }
+                    }
+                });
+
+                MessageBox.Show($"İşlem Tamamlandı! {oda.Numara} Numaralı Oda Rezerve edildi!");
+
+                listBoxOdalar.Items.Remove(oda);
+            }
+        }
+
+        private void btnRezervasyonList_Click(object sender, EventArgs e)
+        {
+            labelList.Text = "Seçili Tarih Rez. (Detay İçin Çift Tık!)";
+            listBoxOdalar.Items.Clear();
+
+            foreach (var oda in otel.Odalari)
+            {
+                if (oda.Rezervasyonlar != null && oda.Rezervasyonlar.Count > 0)
+                {
+                    foreach (var item in oda.Rezervasyonlar)
+                    {
+                        if (item.GirisTarihi.Date == dateTimePickerBas.Value.Date)
+                        {
+                            listBoxOdalar.Items.Add(oda);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void listBoxOdalar_DoubleClick(object sender, EventArgs e)
+        {
+            if (labelList.Text == "Tüm Rezervasyonlar (Çift Tık: Sil)")
+            {
+                // Rezervasyon İptali
+                var rez = listBoxOdalar.SelectedItem as Rezervasyon;
+
+                var state = otel.RezervasyonIptal(rez);
+                if (state)
+                {
+                    listBoxOdalar.Items.Remove(rez);
+                }
+            }
+            else
+            {
+                if (listBoxOdalar.SelectedItem == null)
+                {
+                    MessageBox.Show("Oda Seçiniz!");
+                }
+                else
+                {
+                    var oda = listBoxOdalar.SelectedItem as Oda;
+
+                    if (oda.Rezervasyonlar != null && oda.Rezervasyonlar.Count > 0)
+                    {
+                        var rezervasyon = oda.Rezervasyonlar.FirstOrDefault(x => x.GirisTarihi.Date == dateTimePickerBas.Value.Date);
+                        if (rezervasyon != null)
+                        {
+                            MessageBox.Show($"Rezervasyon Bilgileri:" +
+                                $"\n\nTarihler:\n{rezervasyon.GirisTarihi.ToString("dd.MM.yyyy")} - {rezervasyon.CikisTarihi.ToString("dd.MM.yyyy")}" +
+                                $"\n\nMisafiler: {rezervasyon.misafiler}");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Seçili Tarihe Ait Rezervasyon Bulunamadı!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Seçili Tarihe Ait Rezervasyon Bulunamadı!");
+                    }
+
+                }
+            }
+        }
+
+        private void dateTimePickerBas_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePickerBit.Value = dateTimePickerBas.Value.AddDays(1);
+        }
+
+        private void btnTumRez_Click(object sender, EventArgs e)
+        {
+            labelList.Text = "Tüm Rezervasyonlar (Çift Tık: Sil)";
+            listBoxOdalar.Items.Clear();
+
+            var date1 = dateTimePickerBas.Value;
+            foreach (var oda in otel.Odalari)
+            {
+                foreach (var item in oda.Rezervasyonlar)
+                {
+                    listBoxOdalar.Items.Add(item);
+                }
+            }
+
+            var oran = otel.DolulukOrani(date1);
+
+            labelOtelAdi.Text = otel.Adi + " (Doluluk Oranı : %" + oran + ")";
+        }
+
+        private void btnDoluluk_Click(object sender, EventArgs e)
+        {
+            var date1 = dateTimePickerBas.Value;
+
+            var oran = otel.DolulukOrani(date1);
+
+            MessageBox.Show("DOLULUK ORANI: %" + oran);
         }
     }
 }
